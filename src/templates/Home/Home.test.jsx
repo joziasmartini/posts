901d8bcdc58250e6ed182780892a380775999040
Jozias Martini <jozias.martini@gmail.com>
2022-03-20 20:08:21 -0300

    Create test file for Home component (without tests)

---
+++ src/templates/Home/Home.test.jsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { Home } from "./index";
+
+describe("<Home />", () => {
+  test("Need to create tests", () => {
+    render(<Home />);
+  });
+});
