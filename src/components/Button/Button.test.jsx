9a40fb10e63be1aa89be9bca5045f248fc1e7a11
Jozias Martini <jozias.martini@gmail.com>
2022-03-20 18:02:48 -0300

    Finish tests for Button component

---
+++ src/components/Button/Button.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Button } from "./index";
+
+describe("<Button />", () => {
+  test("should render a button with text", () => {
+    render(<Button text="Load more" />);
+    const button = screen.getByText(/load more/i);
+    expect(button).toBeInTheDocument();
+  });
+
+  test("should call function on button click", () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" onClick={fn} />);
+    const button = screen.getByText(/load more/i);
+    userEvent.click(button);
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  test("should be disabled when disabled is true", () => {
+    render(<Button text="Load more" disabled />);
+    const button = screen.getByText(/load more/i);
+    expect(button).toBeDisabled();
+  });
+
+  test("should be enabled when disabled is false", () => {
+    render(<Button text="Load more" disabled={false} />);
+    const button = screen.getByText(/load more/i);
+    expect(button).toBeEnabled();
+  });
+});
